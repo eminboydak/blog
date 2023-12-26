69d6545e1b71c981403bb41222c0c6476ac452c2
Muhammed Emin Boydak <emnbydk@gmail.com>
2023-12-27 00:26:45 +0300

    astro a new era :)

---
+++ src/site.config.ts
@@ -0,0 +1,42 @@
+import type { SiteConfig } from "@/types";
+
+export const siteConfig: SiteConfig = {
+    // Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
+    author: "Muhammed Emin Boydak",
+    // Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
+    title: "emin's logbook",
+    // Meta property used as the default description meta property
+    description: " ",
+    // HTML lang property, found in src/layouts/Base.astro L:18
+    lang: "en-US",
+    // Meta property, found in src/components/BaseHead.astro L:42
+    ogLocale: "en_US",
+    // Date.prototype.toLocaleDateString() parameters, found in src/utils/date.ts.
+    date: {
+        locale: "en-US",
+        options: {
+            day: "numeric",
+            month: "short",
+            year: "numeric",
+        },
+    },
+    webmentions: {
+        link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+    },
+};
+
+// Used to generate links in both the Header & Footer.
+export const menuLinks: Array<{ title: string; path: string }> = [
+    {
+        title: "home",
+        path: "/",
+    },
+    {
+        title: "about",
+        path: "/about/",
+    },
+    {
+        title: "blog",
+        path: "/posts/",
+    },
+];
