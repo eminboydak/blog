94a289f8aa51a06d3261bb30ef4f4d2f318b069e
Muhammed Emin Boydak <emnbydk@gmail.com>
2024-03-07 00:57:32 +0300

    site: Edited webmention

--- src/site.config.ts
+++ src/site.config.ts
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
         },
     },
     webmentions: {
-        link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+        link: "https://webmention.io/eminboydak.com/webmention",
     },
 };
 
